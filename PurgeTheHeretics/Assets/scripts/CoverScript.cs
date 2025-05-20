5dd695358fa39fe864cb62eb835b385a7d7c01b1
Joadlaen <jackcullenmurphy1999@gmail.com>
2025-05-20 20:02:22 +0100

    Commented CoverScript.cs

--- PurgeTheHeretics/Assets/scripts/CoverScript.cs
+++ PurgeTheHeretics/Assets/scripts/CoverScript.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//an initial idea was to add cover mechanics which included the red sprite.
+//it would have similar effects to warhammer cover Terrain which block only vehicles 
+//it would also affect armour saves by adding one to the roll.
+//armour saves weren't implemented and the idea was discarded early in production.
 public class CoverScript : MonoBehaviour
 {
     // Start is called before the first frame update
