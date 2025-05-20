29437238a4b4db73e45ad14696b9e4bfadc70583
Joadlaen <jackcullenmurphy1999@gmail.com>
2025-05-20 20:08:00 +0100

    Commented Dice.cs

--- PurgeTheHeretics/Assets/scripts/Dice.cs
+++ PurgeTheHeretics/Assets/scripts/Dice.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    // I originally wanted to implement a central dice system
     void Start()
     {
         
