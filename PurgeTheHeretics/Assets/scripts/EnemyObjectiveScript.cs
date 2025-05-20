66d5e804e17d024c1af8d4c2dffcf275a610c612
Joadlaen <jackcullenmurphy1999@gmail.com>
2025-05-20 20:16:08 +0100

    Commented EnemyObjectiveScript.cs

--- PurgeTheHeretics/Assets/scripts/EnemyObjectiveScript.cs
+++ PurgeTheHeretics/Assets/scripts/EnemyObjectiveScript.cs
@@ -5,13 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class EnemyObjectiveScript : MonoBehaviour
 {
+// before I simplified the win condition, this was intended to activate the win condition
+// it didn't work as intended which is why the other win condition script exists.
     public void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "HomeTank" || other.tag == "HomeSquad" && 
             other.transform.position.x == this.transform.position.x && 
             other.transform.position.y == this.transform.position.y)
         {
-            SceneManager.LoadScene("HomeWins");
+            SceneManager.LoadScene("homeWins");
         }
 
 
